1543f977502f67aaad1d7c5eb058c256d5d2e281
Luis Guerreiro <luiscvlh11@gmail.com>
2023-10-13 23:05:00 -0300

    refactor(django admin): :wrench: removido tabela de vendas do django admin

--- paper/core/admin.py
+++ paper/core/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
-from paper.core.domain.admin.sale import SaleAdmin
 from paper.core.domain.entity.client import Client
 from paper.core.domain.entity.product import Product
-from paper.core.domain.entity.sale import Sale
 from paper.core.domain.entity.seller import Seller
 
+# from paper.core.domain.entity.sale import Sale
+# from paper.core.domain.admin.sale import SaleAdmin
+
 admin.site.register(Product)
-admin.site.register(Sale, SaleAdmin)
 admin.site.register(Client)
 admin.site.register(Seller)
+
+# admin.site.register(Sale, SaleAdmin)
