db3d84746971ce81901b9934918378edccac5103
Luis Guerreiro <luiscvlh11@gmail.com>
2023-10-17 01:33:22 -0300

    fix(sale): :bug: corrigido listagem das vendas

--- paper/core/infra/repository/sale.py
+++ paper/core/infra/repository/sale.py
@@ -6,7 +6,7 @@ from paper.core.domain.repository.sale import ISaleRepository
 
 class SaleRepository(ISaleRepository):
     def get_all(self) -> List[Sale]:
-        return Sale.objects.select_related("client", "seller").prefetch_related("items__product").all()
+        return list(Sale.objects.select_related("client", "seller").prefetch_related("items__product").all())
 
     def get_by_id(self, pk: int) -> Sale:
         return Sale.objects.get(pk=pk)
