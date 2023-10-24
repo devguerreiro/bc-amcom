724818d6b906f54e087761ad10e6930ea84bc54c
Luis Guerreiro <luiscvlh11@gmail.com>
2023-10-24 06:18:29 -0300

    fix: :bug: ajustado allowed hosts

--- dunder_mifflin/settings.py
+++ dunder_mifflin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["api", "localhost", "127.0.0.1"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
